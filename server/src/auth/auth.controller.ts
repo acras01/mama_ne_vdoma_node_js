512e3b2da422834ae7beee136b49d22aebe9828c
Ivan <46397494+Simkav@users.noreply.github.com>
2023-10-01 15:00:26 +0300

    Fix auth (#3)
    * remove port from env
    
    * add dto for delete parent route
    
    * delete parent rout
    
    * remove old not confirmed profile on registration

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { ParentService } from 'src/Parent/parent.service'
 import {
   Body,
   Controller,
@@ -5,53 +6,64 @@ import {
   HttpCode,
   Post,
   UseGuards,
-} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { RegisterDto } from './dto/register.dto';
-import { ConfirmEmailDto } from './dto/confirm-email.dto';
+} from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { RegisterDto } from './dto/register.dto'
+import { ConfirmEmailDto } from './dto/confirm-email.dto'
 import {
   ApiBearerAuth,
   ApiCreatedResponse,
   ApiOkResponse,
   ApiTags,
-} from '@nestjs/swagger';
-import { LoginDto } from './dto/login.dto';
-import { UserData } from './decorators/get-user-from-jwt.decorator';
-import { IJwtData } from 'src/shared/interfaces/jwt-data.interface';
-import { AuthGuard } from './guards/auth.guards';
+} from '@nestjs/swagger'
+import { LoginDto } from './dto/login.dto'
+import { UserData } from './decorators/get-user-from-jwt.decorator'
+import { IJwtData } from 'src/shared/interfaces/jwt-data.interface'
+import { AuthGuard } from './guards/auth.guards'
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor (
+    private readonly authService: AuthService,
+    private readonly parentService: ParentService,
+  ) {}
 
   @Post('login')
   @HttpCode(200)
   @ApiOkResponse()
-  async login(@Body() loginDto: LoginDto): Promise<{ jwt: string }> {
-    const jwt = await this.authService.login(loginDto);
-    return { jwt };
+  async login (@Body() loginDto: LoginDto): Promise<{ jwt: string }> {
+    const jwt = await this.authService.login(loginDto)
+    return { jwt }
   }
 
   @Post('register')
   @ApiCreatedResponse()
-  async register(@Body() registerDto: RegisterDto) {
-    await this.authService.register(registerDto);
+  async register (@Body() registerDto: RegisterDto) {
+    try {
+      const user = await this.parentService.findFullInfoByEmail(
+        registerDto.email,
+      )
+      if (!user.isConfirmed) {
+        await user.deleteOne()
+      }
+    } catch (error) {}
+    await this.authService.register(registerDto)
   }
 
   @Post('confirm')
   @HttpCode(200)
   @ApiOkResponse()
-  async confirmAccount(@Body() confirmEmailDto: ConfirmEmailDto) {
-    const jwt = await this.authService.confirmEmail(confirmEmailDto);
-    return { jwt };
+  async confirmAccount (@Body() confirmEmailDto: ConfirmEmailDto) {
+    const jwt = await this.authService.confirmEmail(confirmEmailDto)
+    return { jwt }
   }
 
   @Get('me')
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @ApiOkResponse()
-  async getMe(@UserData() jwtData: IJwtData) {
-    return await this.authService.getMe(jwtData.email);
+  async getMe (@UserData() jwtData: IJwtData) {
+    return await this.authService.getMe(jwtData.email)
   }
 }
