58be8af7afde727370c452ca2409d6d19971261b
Ivan <simkav2411@gmail.com>
2023-10-01 14:57:37 +0300

    delete parent rout

--- server/src/Parent/parent.controller.ts
+++ server/src/Parent/parent.controller.ts
@@ -1,44 +1,54 @@
 import {
   Body,
   Controller,
+  Delete,
   HttpCode,
   Patch,
   Post,
   UseGuards,
-} from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from 'src/auth/guards/auth.guards';
-import { ParentService } from 'src/Parent/parent.service';
-import { PatchParentDto } from './dto/patch-parent.dto';
-import { UserData } from 'src/auth/decorators/get-user-from-jwt.decorator';
-import { IJwtData } from '../shared/interfaces/jwt-data.interface';
-import { UpdateGeoDto } from 'src/shared/dto/update-geo.dto';
+} from '@nestjs/common'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { AuthGuard } from 'src/auth/guards/auth.guards'
+import { ParentService } from 'src/Parent/parent.service'
+import { PatchParentDto } from './dto/patch-parent.dto'
+import { UserData } from 'src/auth/decorators/get-user-from-jwt.decorator'
+import { IJwtData } from '../shared/interfaces/jwt-data.interface'
+import { UpdateGeoDto } from 'src/shared/dto/update-geo.dto'
+import { DeleteParentDto } from './dto/delete-parent.dto'
 
-@ApiBearerAuth()
-@UseGuards(AuthGuard)
 @ApiTags('parent')
 @Controller('parent')
 export class ParentController {
-  constructor(private readonly parentService: ParentService) {}
+  constructor (private readonly parentService: ParentService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Patch()
   @HttpCode(200)
-  async updateParent(
+  async updateParent (
     @Body() patchParentDto: PatchParentDto,
     @UserData() data: IJwtData,
   ) {
-    return await this.parentService.updateParent(patchParentDto, data.email);
+    return await this.parentService.updateParent(patchParentDto, data.email)
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   @Patch('geo')
   @HttpCode(200)
-  async updateParentGeo(
+  async updateParentGeo (
     @Body() updateGeoDto: UpdateGeoDto,
     @UserData() jwtData: IJwtData,
   ) {
     return await this.parentService.updateGeoLocation(
       updateGeoDto,
       jwtData.email,
-    );
+    )
+  }
+
+  
+  @Delete()
+  async deleteParent (@Body() deleteParentDto: DeleteParentDto) {
+    return await this.parentService.deleteParent(deleteParentDto.email)
   }
 }
--- server/src/Parent/parent.service.ts
+++ server/src/Parent/parent.service.ts
@@ -1,64 +1,69 @@
-import { InjectModel } from '@m8a/nestjs-typegoose';
-import { Parent } from './models/parent.model';
-import { ReturnModelType } from '@typegoose/typegoose';
-import * as bcrypt from 'bcrypt';
-import { CreateParentDto } from './dto/create-parent.dto';
-import { MailService } from '../mail/mail.service';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
-import { PatchParentDto } from './dto/patch-parent.dto';
-import { ConfirmEmailDto } from '../auth/dto/confirm-email.dto';
-import { UpdateGeoDto } from '../shared/dto/update-geo.dto';
+import { InjectModel } from '@m8a/nestjs-typegoose'
+import { Parent } from './models/parent.model'
+import { ReturnModelType } from '@typegoose/typegoose'
+import * as bcrypt from 'bcrypt'
+import { CreateParentDto } from './dto/create-parent.dto'
+import { MailService } from '../mail/mail.service'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { PatchParentDto } from './dto/patch-parent.dto'
+import { ConfirmEmailDto } from '../auth/dto/confirm-email.dto'
+import { UpdateGeoDto } from '../shared/dto/update-geo.dto'
 
 export class ParentService {
-  constructor(
+  constructor (
     @InjectModel(Parent)
     private readonly parentModel: ReturnModelType<typeof Parent>,
     private readonly mailService: MailService,
   ) {}
 
-  async findByEmail(email: string) {
-    const findedDoc = await this.parentModel.findOne({ email });
-    if (findedDoc === null) throw new NotFoundException('Email not found');
-    return findedDoc;
+  async findByEmail (email: string) {
+    const findedDoc = await this.parentModel.findOne({ email })
+    if (findedDoc === null) throw new NotFoundException('Email not found')
+    return findedDoc
   }
 
-  async findFullInfoByEmail(email: string) {
+  async findFullInfoByEmail (email: string) {
     const findedDoc = await this.parentModel
       .findOne({ email })
-      .populate(['password', 'activationCode']);
-    if (findedDoc === null) throw new NotFoundException('Email not found');
-    return findedDoc;
+      .populate(['password', 'activationCode'])
+    if (findedDoc === null) throw new NotFoundException('Email not found')
+    return findedDoc
   }
 
-  async createParent(createParentDto: CreateParentDto) {
-    const hash = await bcrypt.hash(createParentDto.password, 10);
-    const activationCode = String(this.generateFourDigitCode());
+  async createParent (createParentDto: CreateParentDto) {
+    const hash = await bcrypt.hash(createParentDto.password, 10)
+    const activationCode = String(this.generateFourDigitCode())
     await this.parentModel.create({
       password: hash,
       email: createParentDto.email,
       activationCode,
-    });
+    })
     await this.mailService.sendConfirmationEmail({
       code: activationCode,
       email: createParentDto.email,
-    });
-    return true;
+    })
+    return true
   }
 
-  async confirmAccountByCode(confirmEmailDto: ConfirmEmailDto) {
-    const parent = await this.findFullInfoByEmail(confirmEmailDto.email);
+  async confirmAccountByCode (confirmEmailDto: ConfirmEmailDto) {
+    const parent = await this.findFullInfoByEmail(confirmEmailDto.email)
     if (parent.activationCode !== confirmEmailDto.code) {
-      throw new BadRequestException('Wrong code');
+      throw new BadRequestException('Wrong code')
     }
-    parent.isConfirmed = true;
-    parent.activationCode = '';
-    await parent.save();
-    return true;
+    parent.isConfirmed = true
+    parent.activationCode = ''
+    await parent.save()
+    return true
   }
 
-  async updateGeoLocation(updateGeoDto: UpdateGeoDto, email: string) {
-    const parent = await this.findByEmail(email);
-    await parent.updateOne({ location: { type: 'Point', coordinates: [updateGeoDto.lon, updateGeoDto.lat] } });
+  async updateGeoLocation (updateGeoDto: UpdateGeoDto, email: string) {
+    const parent = await this.findByEmail(email)
+    await parent.updateOne({
+      location: {
+        type: 'Point',
+        coordinates: [updateGeoDto.lon, updateGeoDto.lat],
+      },
+    })
     // console.log(parent.location);
     // parent.location.type = 'Point';
     // parent.location.coordinates = ;
@@ -66,14 +71,25 @@ export class ParentService {
     // await parent.save();
   }
 
-  async updateParent(patchParentDto: PatchParentDto, email: string) {
-    const parent = await this.findByEmail(email);
-    await parent.updateOne(patchParentDto);
-    return await this.findByEmail(email);
+  async updateParent (patchParentDto: PatchParentDto, email: string) {
+    const parent = await this.findByEmail(email)
+    await parent.updateOne(patchParentDto)
+    return await this.findByEmail(email)
   }
 
-  private generateFourDigitCode() {
-    const randomNumber = Math.floor(Math.random() * 9000) + 1000;
-    return randomNumber;
+  async deleteParent (email: string) {
+    // todo add security
+    const parent = await this.parentModel.findOne({ email })
+    if (parent === null) {
+      return false
+    } else {
+      await parent.deleteOne()
+      return true
+    }
+  }
+
+  private generateFourDigitCode () {
+    const randomNumber = Math.floor(Math.random() * 9000) + 1000
+    return randomNumber
   }
 }
