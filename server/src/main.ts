48eaedb30cc9c78da62ec18d804b4f134c7264a4
Ivan Khomenko <simkav2411@gmail.com>
2023-12-04 22:01:31 +0200

    add basic logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,9 +8,45 @@ import passport from 'passport';
 import MongoStore from 'connect-mongo';
 import { ConfigService } from '@nestjs/config';
 import { IEnv } from './configs/env.config';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        // file on daily rotation (error only)
+        new transports.DailyRotateFile({
+          // %DATE will be replaced by the current date
+          filename: `../logs/%DATE%-error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false, // don't want to zip our logs
+          maxFiles: '30d', // will keep log until they are older than 30 days
+        }),
+        // same for all levels
+        new transports.DailyRotateFile({
+          filename: `../logs/%DATE%-combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: false,
+          maxFiles: '30d',
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
   const configService = app.get(ConfigService<IEnv>);
   const globalPrefix = configService.get('GLOBAL_PREFIX');
   app.set('trust proxy', 1);
